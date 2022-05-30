ac7ea0f8e9929fa5b8d196d5618b0368fbf99c52
flyingfrisbee <giovannhiu@gmail.com>
2022-05-30 23:12:41 +0700

    structuring the application packages

--- app/src/main/java/com/giovann/composenotetesting/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/giovann/composenotetesting/feature_note/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.giovann.composenotetesting
+package com.giovann.composenotetesting.feature_note.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -15,24 +15,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeNoteTestingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeNoteTestingTheme {
-        Greeting("Android")
     }
 }
