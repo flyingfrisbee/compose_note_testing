3aff7ae49bfccd69f4d0ccae8e3da7a7831cd239
flyingfrisbee <giovannhiu@gmail.com>
2022-06-05 20:36:27 +0700

    created TestAppModule and test version of @HiltAndroidApp

---
+++ app/src/androidTest/java/com/giovann/composenotetesting/feature_note/presentation/notes/NotesScreenTest.kt
@@ -0,0 +1,12 @@
+package com.giovann.composenotetesting.feature_note.presentation.notes
+
+import com.giovann.composenotetesting.di.AppModule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Assert.*
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class NotesScreenTest {
+
+}
